4cba323a4948c84742eb662ccf95589e50aaf0f3
lingshin <yokaringx@outlook.com>
2025-10-31 11:45:30 +0800

    fix(kitty): fix syntax error

--- home/features/applications/kitty.nix
+++ home/features/applications/kitty.nix
@@ -1,9 +1,7 @@
-{ lib, ... }:
-
-let
+{lib, ...}: let
   leader_key = "alt+space";
   leader = key: leader_key + ">" + key;
-  mode = mode_name: key: lib.strings.join " " ["--mode" , mode_name , key];
+  mode = mode_name: key: lib.strings.join " " ["--mode" mode_name key];
   resize = mode "resize";
   new_mode = mode_name: "--new-mode " + mode_name;
 in {
@@ -22,12 +20,11 @@ in {
       window_margin_width = "1";
       confirm_os_window_close = "0";
       shell = "fish";
-      scrollback_pager = "nvim -c 'set filetype=scrollback'"
+      scrollback_pager = "nvim -c 'set filetype=scrollback'";
     };
 
     keybindings = {
-      "ctrl+f" =
-        "launch --location=hsplit --allow-remote-control kitty +kitten search.py @active-kitty-window-id";
+      "ctrl+f" = "launch --location=hsplit --allow-remote-control kitty +kitten search.py @active-kitty-window-id";
       "page_up" = "scroll_page_up";
       "page_down" = "scroll_page_down";
 
@@ -46,7 +43,7 @@ in {
       ${resize "shift+space"} = "scroll_page_up";
       ${resize "shift+bracketleft"} = "scroll_to_prompt -1";
       ${resize "shift+bracketright"} = "scroll_to_prompt 1";
-      ${resize "plus"} =  "change_font_size all +2.0";
+      ${resize "plus"} = "change_font_size all +2.0";
       ${resize "minus"} = "change_font_size all -2.0";
       ${resize "equal"} = "change_font_size all 0";
     };
