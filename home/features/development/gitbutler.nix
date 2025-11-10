5b9a2dabbaaae21aedb3dee679957df76b9d59f2
yb <scauyb@qq.com>
2025-11-11 05:54:11 +0800

    chore(deps): 移除过时的 gitbutler 包
    -从 home.packages 中移除 gitbutler- 移除不再需要的 appimage-run依赖
    - 更新函数参数格式，移除多余空格

--- home/features/development/gitbutler.nix
+++ home/features/development/gitbutler.nix
@@ -1,5 +1,4 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   # maybe it's a good way to manage my git repos
   home.packages = with pkgs; [
     # now don't use it , version is too old in nixos, and can't use it by other way
@@ -8,7 +7,6 @@
     #    xorg.libX11
     #    xorg.libXext
     #    xorg.libXrender
-    appimage-run
-    gitbutler
+    # gitbutler
   ];
 }
