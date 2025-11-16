495f3a419308e35fb53f0399bd5ccdc0fc821a5e
Sibyl <scauyb@qq.com>
2025-11-17 05:57:17 +0800

    add jj in git

--- home/features/development/git.nix
+++ home/features/development/git.nix
@@ -2,26 +2,51 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  name = "Sibyl";
+  email = "scauyb@qq.com";
+in {
   home.packages = with pkgs; [
     lazygit
-    gitui
     gh
+
     jujutsu
-    lazyjj
+    jjui
+    meld
+    delta
   ];
-  programs.git = {
-    enable = true;
-    settings = {
-      user.name = "yb";
-      user.email = "scauyb@qq.com";
+  programs = {
+    git = {
+      enable = true;
+      settings = {
+        user.name = name;
+        user.email = email;
 
-      gpg.format = "ssh";
-      user.signingkey = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
-      commit.gpgsign = true;
-      gpg.ssh.allowedSignersFile = "${config.home.homeDirectory}/.ssh/allowed_signers";
+        gpg.format = "ssh";
+        user.signingkey = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
+        commit.gpgsign = true;
+        gpg.ssh.allowedSignersFile = "${config.home.homeDirectory}/.ssh/allowed_signers";
 
-      url."git@github.com:".insteadOf = "https://github.com/";
+        url."git@github.com:".insteadOf = "https://github.com/";
+      };
+    };
+
+    jujutsu = {
+      enable = true;
+      settings = {
+        user.name = name;
+        user.email = email;
+        ui.editor = "nvim";
+        ui.diff-editor = "meld";
+        merge.tool = "meld";
+        diff.tool = "delta";
+        ui.default-command = "log";
+
+        git.remote = "origin";
+        git.auto-local-branch = true;
+        git.fetch = true;
+        git.push-branch-prefix = "";
+      };
     };
   };
 }
