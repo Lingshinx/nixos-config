cc9b014eff8de61a69066fe4d14ef32d1ea6bdb0
yb <scauyb@qq.com>
2025-11-16 10:09:59 +0800

    feat(niri): 调整窗口规则配置格式
    - 移除了窗口规则中的换行符
    - 统一了配置项的缩进格式
    - 确保spawn-at-startup命令独立成行
    - 优化了模板字符串的可读性
    
    feat(niri): 增强窗口规则配置功能
    
    - 引入 opacity 和 default-column-width 等额外参数支持
    - 支持通过 app_id 更精确匹配应用
    - 为网易云音乐、Telegram 和 piliplus 配置透明度及列宽规则
    - 提升桌面环境自定义能力和视觉体验一致性
    
    feat(niri): 配置钉钉和娱乐应用自动启动及工作区分配
    
    - 为钉钉配置自动启动并分配至 9-fun 工作区
    - 为网易云音乐、Telegram 和哔哩哔哩配置自动启动并分配至 5-fun 工作区
    - 提取窗口规则函数以复用配置逻辑
    - 移除旧的包管理配置方式
    - 更新 Niri 配置文件结构以支持动态生成

--- home/features/desktop/niri/co1_niri.nix
+++ home/features/desktop/niri/co1_niri.nix
@@ -1,4 +1,16 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-  ];
+{...}: let
+  fn_windows = app_name: workspace_name: ''
+    spawn-at-startup "${app_name}"
+    window-rule {
+        match app-id="${app_name}"
+        open-on-workspace "${workspace_name}";
+    }
+  '';
+in {
+  xdg.configFile."niri/config.kdl".text =
+    builtins.readFile ./config.kdl
+    + ''
+      ${fn_windows "dingtalk" "9-fun"}
+
+    '';
 }
--- home/features/desktop/niri/pc1_niri.nix
+++ home/features/desktop/niri/pc1_niri.nix
@@ -1,6 +1,42 @@
-{ ... }:
-{
-  xdg.configFile."niri/config.kdl".text = builtins.readFile ./config.kdl + ''
-    // extra lines hh
+{...}: let
+  # 定义一个函数，接受四个参数，其中 is_opacity 默认 false
+  fn_windows = {
+    app_name,
+    app_id,
+    workspace_name,
+    extra_args ? "",
+  }: ''
+    window-rule {
+      match app-id="${app_id}"
+      open-on-workspace "${workspace_name}"
+      ${extra_args}
+    }
+    spawn-at-startup "${app_name}"
   '';
+in {
+  xdg.configFile."niri/config.kdl".text =
+    builtins.readFile ./config.kdl
+    + ''
+      ${fn_windows {
+        app_name = "Telegram";
+        app_id = "org.telegram.desktop";
+        workspace_name = "5-fun";
+      }}
+      ${
+        fn_windows {
+          app_name = "netease-cloud-music-gtk4";
+          app_id = "com.gitee.gmg137.NeteaseCloudMusicGtk4";
+          workspace_name = "5-fun";
+          extra_args = "opacity 0.75";
+        }
+      }
+      ${fn_windows {
+        app_name = "piliplus";
+        app_id = "piliplus";
+        workspace_name = "5-fun";
+        extra_args = ''
+          default-column-width {proportion 0.99;}
+        '';
+      }}
+    '';
 }
