cbaff23793360ef2e27d2bcc74c8f3b3bea81833
Sibyl <scauyb@qq.com>
2025-11-18 11:49:26 +0800

    feat(rust): configure rustup with custom cargo config
    - Add custom cargo configuration for clang linker
    - Set up rustup initialization with stable toolchain
    - Include rustfmt and clippy components installation
    - Create .cargo directory during activation
    - Copy generated cargo config to user directory

--- home/features/development/rust.nix
+++ home/features/development/rust.nix
@@ -2,7 +2,12 @@
   pkgs,
   lib,
   ...
-}: {
+}: let
+  rustupConfig = pkgs.writeText "cargo-config.toml" ''
+    [target.x86_64-unknown-linux-gnu]
+    linker = "${pkgs.clang}/bin/clang"
+  '';
+in {
   home.packages = with pkgs; [
     #    (pkgs.rust-bin.stable.latest.default.override {
     #      extensions = [
@@ -17,6 +22,9 @@
   ];
 
   home.activation.rustupInit = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    ${pkgs.fish}/bin/fish ${./rust/init.fish}
+    mkdir -p $HOME/.cargo
+    cp ${rustupConfig} $HOME/.cargo/config.toml
+    ${pkgs.rustup}/bin/rustup install stable
+    ${pkgs.rustup}/bin/rustup component add rustfmt clippy rust-analyzer
   '';
 }
